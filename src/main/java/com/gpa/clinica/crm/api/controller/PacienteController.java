88f470d5458b68c3b6734cbd82e9b83a37504298
Evandro Gubert <gubertevandro33@gmail.com>
2026-01-08 22:44:34 -0300

    feature: iniciando criação de controller de paciente

--- src/main/java/com/gpa/clinica/crm/api/controller/PacienteController.java
+++ src/main/java/com/gpa/clinica/crm/api/controller/PacienteController.java
@@ -1,4 +1,20 @@
 package com.gpa.clinica.crm.api.controller;
 
-public class PacienteController {
+import com.gpa.clinica.crm.api.controller.openapi.PacienteControllerOpenAPI;
+import com.gpa.clinica.crm.api.model.CadastrarPacienteRequest;
+import com.gpa.clinica.crm.api.model.CadastrarPacienteResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pacientes")
+@RequiredArgsConstructor
+public class PacienteController implements PacienteControllerOpenAPI {
+
+
+    @Override
+    public CadastrarPacienteResponse cadastrarPaciente(CadastrarPacienteRequest cadastrarPacienteRequest) {
+        return null;
+    }
 }
