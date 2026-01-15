59373cc2796943cf415a55f3fe5f90e38fdaac40
Evandro Gubert <gubertevandro33@gmail.com>
2026-01-15 18:07:08 -0300

    fix: correcao de migration de conversa e organizacao de imports

--- src/main/resources/db/migration/V3__criar-tabela-conversa.sql
+++ src/main/resources/db/migration/V3__criar-tabela-conversa.sql
@@ -3,8 +3,10 @@ CREATE TABLE conversa
     id          VARCHAR(36) NOT NULL,
     conteudo    NVARCHAR(MAX) NOT NULL,
     paciente_id VARCHAR(36) NOT NULL,
+    usuario_id  VARCHAR(36) NOT NULL
 
     CONSTRAINT PK_conversa PRIMARY KEY (id),
 
-    CONSTRAINT  FK_conversa_paciente FOREIGN KEY (paciente_id) REFERENCES paciente(id)
+    CONSTRAINT  FK_conversa_paciente FOREIGN KEY (paciente_id) REFERENCES paciente(id),
+    CONSTRAINT  FK_conversa_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
